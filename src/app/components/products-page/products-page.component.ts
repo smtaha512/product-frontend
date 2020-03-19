ede7a332d214127f4ab3bfd5a201b8e7d2f5abbf
Syed M. Taha <s.m.taha10@gmail.com>
2020-03-19 15:17:46 +0500

    feat(products): add filteration

--- src/app/components/products-page/products-page.component.ts
+++ src/app/components/products-page/products-page.component.ts
@@ -1,5 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import * as moment from 'moment';
+import { Moment } from 'moment';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 import { Product } from 'src/app/models/product';
 import { ProductsService } from 'src/app/services/products/products.service';
@@ -11,14 +14,32 @@ import { ProductsService } from 'src/app/services/products/products.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductsPageComponent implements OnInit, OnDestroy {
+  readonly productsSubject$: BehaviorSubject<Product[]> = new BehaviorSubject([]);
   readonly products$ = this.productsService.read();
   private readonly subscriptions = new Subscription();
+  readonly selectedDates$: BehaviorSubject<{ endDate: Moment; startDate: Moment }> = new BehaviorSubject(null);
+  readonly productsToShow$ = this.productsSubject$.pipe(
+    filter(products => !!products.length),
+    switchMap(products =>
+      this.selectedDates$.pipe(
+        map(dates => (!dates ? products : products.filter(product => moment(product.createdAt).isBetween(dates.startDate, dates.endDate))))
+      )
+    )
+  );
+
   constructor(private readonly productsService: ProductsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscriptions.add(this.products$.subscribe(products => this.productsSubject$.next(products)));
+  }
 
   onCreateProduct(product: Pick<Product, 'name' | 'price'>) {
-    this.subscriptions.add(this.productsService.create(product).subscribe());
+    this.subscriptions.add(
+      this.productsService
+        .create(product)
+        .pipe(switchMap(_ => this.products$))
+        .subscribe(products => this.productsSubject$.next(products))
+    );
   }
 
   ngOnDestroy() {
--- src/app/components/products-page/products-page.module.ts
+++ src/app/components/products-page/products-page.module.ts
@@ -5,9 +5,16 @@ import { RouterModule } from '@angular/router';
 import { ProductsPageComponent } from './products-page.component';
 import { ProductFormModule } from '../product-form/product-form.module';
 import { ProductTableModule } from '../product-table/product-table.module';
+import { ProductsFilterModule } from '../products-filter/products-filter.module';
 
 @NgModule({
   declarations: [ProductsPageComponent],
-  imports: [CommonModule, RouterModule.forChild([{ path: '', component: ProductsPageComponent }]), ProductFormModule, ProductTableModule]
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{ path: '', component: ProductsPageComponent }]),
+    ProductFormModule,
+    ProductTableModule,
+    ProductsFilterModule
+  ]
 })
 export class ProductsPageModule {}
